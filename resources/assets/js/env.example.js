e85b0bb6214fe885b950c1fc5dd2759f43692694
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2017-02-14 20:34:18 +0100

    [TUBDMP-15] Added an env.example.js file for path information

---
+++ resources/assets/js/env.example.js
@@ -0,0 +1,2 @@
+var url = 'http:\/\/dmp.localhost\/';
+var path = '\/srv\/tub-dmp\/';
--- resources/assets/js/env.js
+++
@@ -1,3 +0,0 @@
-//var url = 'http:\/\/localhost\/tub-dmp\/public\/';
-var url = 'http:\/\/dmp.tu-berlin.de\/';
-var path = '\/srv\/tub-dmp\/';
