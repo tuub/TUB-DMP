50c1116503e8aa7975dd1a83814a80bc0680ec22
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2017-03-03 15:07:57 +0100

    [TUBDMP-3] Replaced /Form with Form facade.

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -55,8 +55,8 @@ class MacroServiceProvider extends ServiceProvider
             }
             $html = '';
             $html .= $question->prepend;
-            //$html .= \Form::hidden( $input_name . '[]', 'text' );
-            $html .= \Form::Text( $input_name . '[]', $value, array('data-type' => 'text', 'class' => 'question form-control', 'title' => $question->guidance, $read_only ));
+            //$html .= Form::hidden( $input_name . '[]', 'text' );
+            $html .= Form::Text( $input_name . '[]', $value, array('data-type' => 'text', 'class' => 'question form-control', 'title' => $question->guidance, $read_only ));
             $html .= $question->append;
             return $html;
         });
@@ -83,14 +83,14 @@ class MacroServiceProvider extends ServiceProvider
 
             $html = '';
             $html .= $question->prepend;
-            //$html .= \Form::hidden( $input_name . '[]', 'text' );
-            $html .= \Form::Textarea( $input_name . '[]', $value, array('data-type' => 'text', 'rows' => $rows, 'class' => 'question form-control', 'title' => $question->guidance, $read_only ) );
+            //$html .= Form::hidden( $input_name . '[]', 'text' );
+            $html .= Form::Textarea( $input_name . '[]', $value, array('data-type' => 'text', 'rows' => $rows, 'class' => 'question form-control', 'title' => $question->guidance, $read_only ) );
             $html .= $question->append;
             return $html;
         });
 
         /* ID 3: Auswahlbox, einzeilig */
-        \Form::macro('question_select', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_select', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $value = null;
             foreach( $answer as $output ) {
@@ -100,13 +100,13 @@ class MacroServiceProvider extends ServiceProvider
             $question_options = array('' => 'Please choose ...') + QuestionOption::where('question_id','=',$question->id)->pluck('text','text')->toArray();
             $html = '';
             $html .= $question->prepend;
-            $html .= \Form::select($input_name . '[]', $question_options, $value, array('data-type' => 'option', 'class' => 'question form-control' ) );
+            $html .= Form::select($input_name . '[]', $question_options, $value, array('data-type' => 'option', 'class' => 'question form-control' ) );
             $html .= $question->append;
             return $html;
         });
 
         /* ID 4: Auswahlbox, mehrzeilig */
-        \Form::macro('question_multiselect', function( $question, $input_name, Collection $answer = null, $default_value = null  )
+        Form::macro('question_multiselect', function( $question, $input_name, Collection $answer = null, $default_value = null  )
         {
             $value = null;
             foreach( $answer as $output ) {
@@ -116,14 +116,14 @@ class MacroServiceProvider extends ServiceProvider
             $question_options = array('' => 'Please choose ...') + QuestionOption::where('question_id','=',$question['id'])->pluck('text','text');
             $html = '';
             $html .= $question->prepend;
-            //$html .= \Form::hidden( $input_name . '[]', 'option' );
-            $html .= \Form::select($input_name . '[]', $question_options, $value, array('data-type' => 'option', 'class' => 'question form-control','multiple','size' => '5'));
+            //$html .= Form::hidden( $input_name . '[]', 'option' );
+            $html .= Form::select($input_name . '[]', $question_options, $value, array('data-type' => 'option', 'class' => 'question form-control','multiple','size' => '5'));
             $html .= $question->append;
             return $html;
         });
 
         /* ID 5: Liste */
-        \Form::macro('question_list', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_list', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $value = null;
             if( $answer ) {
@@ -132,13 +132,13 @@ class MacroServiceProvider extends ServiceProvider
 
             $html = '';
             $html .= $question->prepend;
-            $html .= \Form::Text( $input_name . '[]', $value, array('data-type' => 'list', 'class' => 'question form-control', 'title' => $question->guidance ) );
+            $html .= Form::Text( $input_name . '[]', $value, array('data-type' => 'list', 'class' => 'question form-control', 'title' => $question->guidance ) );
             $html .= $question->append;
             return $html;
         });
 
         /* ID 6: Checkboxen */
-        \Form::macro('question_checkboxes', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_checkboxes', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $question_options = QuestionOption::where('question_id','=',$question['id'])->pluck('text','text');
             $html = '';
@@ -161,8 +161,8 @@ class MacroServiceProvider extends ServiceProvider
 
                 $html .= '<div class="checkbox">';
                 $html .= '<label>';
-                //$html .= \Form::hidden( $input_name . '[]', 'option' );
-                $html .= \Form::checkbox( $input_name . '[]', $option_value, $checked, array('data-type' => 'option', 'class' => 'question question-checkbox', 'autocomplete' => 'off', 'title' => $question->guidance ) );
+                //$html .= Form::hidden( $input_name . '[]', 'option' );
+                $html .= Form::checkbox( $input_name . '[]', $option_value, $checked, array('data-type' => 'option', 'class' => 'question question-checkbox', 'autocomplete' => 'off', 'title' => $question->guidance ) );
                 $html .= $option_label;
                 $html .= '</label>';
                 $html .= '</div>';
@@ -174,7 +174,7 @@ class MacroServiceProvider extends ServiceProvider
         });
 
         /* ID 7: Radiobuttons */
-        \Form::macro('question_radiobuttons', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_radiobuttons', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $question_options = QuestionOption::where('question_id', '=', $question->id)->pluck('text','text');
 
@@ -207,8 +207,8 @@ class MacroServiceProvider extends ServiceProvider
 
                 $html .= '<div class="radio">';
                 $html .= '<label>';
-                //$html .= \Form::hidden( $input_name . '[]', 'option' );
-                $html .= \Form::radio( $input_name . '[]', $option_value, $checked, array('data-type' => 'option', 'class' => 'question question-radio', 'autocomplete' => 'off', 'title' => $question->guidance ) );
+                //$html .= Form::hidden( $input_name . '[]', 'option' );
+                $html .= Form::radio( $input_name . '[]', $option_value, $checked, array('data-type' => 'option', 'class' => 'question question-radio', 'autocomplete' => 'off', 'title' => $question->guidance ) );
                 $html .= $option_label;
                 if( $option_value == $default_value )
                 {
@@ -222,7 +222,7 @@ class MacroServiceProvider extends ServiceProvider
         });
 
         /* ID 8: Datumsfeld */
-        \Form::macro('question_date', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_date', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $html = '';
             $value = null;
@@ -231,14 +231,14 @@ class MacroServiceProvider extends ServiceProvider
             }
 
             $html .= $question->prepend;
-            //$html .= \Form::hidden( $input_name . '[]', 'text' );
-            $html .= \Form::Text( $input_name . '[]', $value, array('data-type' => 'text', 'class' => 'question question-date form-control', 'title' => $question['guidance'] ) );
+            //$html .= Form::hidden( $input_name . '[]', 'text' );
+            $html .= Form::Text( $input_name . '[]', $value, array('data-type' => 'text', 'class' => 'question question-date form-control', 'title' => $question['guidance'] ) );
             $html .= $question->append;
             return $html;
         });
 
         /* ID 9: Datumsbereich */
-        \Form::macro('question_daterange', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_daterange', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             if( count( $answer ) > 0 )
             {
@@ -253,16 +253,16 @@ class MacroServiceProvider extends ServiceProvider
 
             $html = '';
             $html .= $question->prepend;
-            //$html .= \Form::hidden( $input_name . '[]', 'range' );
-            $html .= \Form::Text( $input_name . '[]', $date_from, array('data-type' => 'range', 'class' => 'question question-daterange form-control', 'title' => $question->guidance ) );
+            //$html .= Form::hidden( $input_name . '[]', 'range' );
+            $html .= Form::Text( $input_name . '[]', $date_from, array('data-type' => 'range', 'class' => 'question question-daterange form-control', 'title' => $question->guidance ) );
             $html .= ' - ';
-            $html .= \Form::Text( $input_name . '[]', $date_to, array('data-type' => 'range', 'class' => 'question question-daterange form-control', 'title' => $question->guidance ) );
+            $html .= Form::Text( $input_name . '[]', $date_to, array('data-type' => 'range', 'class' => 'question question-daterange form-control', 'title' => $question->guidance ) );
             $html .= $question->append;
             return $html;
         });
 
         /* ID 10: Wertefeld */
-        \Form::macro('question_value', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_value', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $param_array = explode('|',$question->value);
             $value = null;
@@ -274,8 +274,8 @@ class MacroServiceProvider extends ServiceProvider
             $html = '';
             $html .= $question->prepend;
             $html .= '&nbsp;&nbsp;';
-            //$html .= \Form::hidden( $input_name . '[]', 'text' );
-            $html .= \Form::Text( $input_name . '[]', $answer, array('data-type' => 'text', 'class' => 'slider question', 'data-slider-min' => $param_array[0], 'data-slider-max' => $param_array[1], 'data-slider-step' => $param_array[2], 'data-slider-value' => $value, 'title' => $question->guidance ) );
+            //$html .= Form::hidden( $input_name . '[]', 'text' );
+            $html .= Form::Text( $input_name . '[]', $answer, array('data-type' => 'text', 'class' => 'slider question', 'data-slider-min' => $param_array[0], 'data-slider-max' => $param_array[1], 'data-slider-step' => $param_array[2], 'data-slider-value' => $value, 'title' => $question->guidance ) );
             $html .= '&nbsp;&nbsp;';
             $html .= '<span class="slider-value">' . $answer . '</span>';
             $html .= '&nbsp;&nbsp;';
@@ -284,7 +284,7 @@ class MacroServiceProvider extends ServiceProvider
         });
 
         /* ID 11: Wertebereich */
-        \Form::macro('question_valuerange', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_valuerange', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $param_array = explode('|',$question->value);
 
@@ -303,11 +303,11 @@ class MacroServiceProvider extends ServiceProvider
             $html = '';
             $html .= $question->prepend;
             $html .= '&nbsp;&nbsp;';
-            //$html .= \Form::hidden( $input_name . '[]', 'range' );
-            $html .= \Form::Text( null, null, array('class' => 'slider-range', 'data-slider-min' => $param_array[0], 'data-slider-max' => $param_array[1], 'data-slider-step' => $param_array[2], 'data-slider-value' => '[' . $alpha_omega . ']', 'title' => $question->guidance ) );
+            //$html .= Form::hidden( $input_name . '[]', 'range' );
+            $html .= Form::Text( null, null, array('class' => 'slider-range', 'data-slider-min' => $param_array[0], 'data-slider-max' => $param_array[1], 'data-slider-step' => $param_array[2], 'data-slider-value' => '[' . $alpha_omega . ']', 'title' => $question->guidance ) );
             $html .= '&nbsp;&nbsp;';
-            $html .= \Form::hidden( $input_name . '[]', $alpha, array('data-type' => 'range', 'class' => 'question slider-range-input-alpha') );
-            $html .= \Form::hidden( $input_name . '[]', $omega, array('data-type' => 'range', 'class' => 'question slider-range-input-omega') );
+            $html .= Form::hidden( $input_name . '[]', $alpha, array('data-type' => 'range', 'class' => 'question slider-range-input-alpha') );
+            $html .= Form::hidden( $input_name . '[]', $omega, array('data-type' => 'range', 'class' => 'question slider-range-input-omega') );
             $html .= '<span class="slider-range-display-alpha">' . $alpha . '</span>';
             $html .= ' - ';
             $html .= '<span class="slider-range-display-omega">' . $omega . '</span>';
@@ -317,7 +317,7 @@ class MacroServiceProvider extends ServiceProvider
         });
 
         /* ID 12: Ja / Nein */
-        \Form::macro('question_boolean', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_boolean', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $question_options = QuestionOption::where('question_id', '=', $question['id'])->pluck('text', 'id');
             $html = '';
@@ -335,8 +335,8 @@ class MacroServiceProvider extends ServiceProvider
                 }
                 $html .= '<div class="radio">';
                 $html .= '<label>';
-                //$html .= \Form::hidden($input_name . '[]', 'option');
-                $html .= \Form::radio($input_name . '[]', $option_value, $checked, array('data-type' => 'option', 'class' => 'question question-radio', 'autocomplete' => 'off', 'title' => $question->guidance));
+                //$html .= Form::hidden($input_name . '[]', 'option');
+                $html .= Form::radio($input_name . '[]', $option_value, $checked, array('data-type' => 'option', 'class' => 'question question-radio', 'autocomplete' => 'off', 'title' => $question->guidance));
                 $html .= $option_label;
                 if ($option_value == $default_value) {
                     $html .= ' ( Standardauswahl )';
@@ -349,7 +349,7 @@ class MacroServiceProvider extends ServiceProvider
         });
 
         /* ID 99: Text */
-        \Form::macro('question_headline', function( $question, $input_name, Collection $answer = null, $default_value = null )
+        Form::macro('question_headline', function( $question, $input_name, Collection $answer = null, $default_value = null )
         {
             $value = null;
             foreach( $answer as $output )
