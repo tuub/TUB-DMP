c809adb97dad630fb506c6155e331e485680745e
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2017-03-03 15:07:40 +0100

    [TUBDMP-3] Removed unnecessary institutions from seed.

--- database/seeds/InstitutionTableSeeder.php
+++ database/seeds/InstitutionTableSeeder.php
@@ -20,32 +20,5 @@ class InstitutionTableSeeder extends Seeder
             'is_external' => 0,
             'is_active' => 1
         ]);
-
-        Institution::create([
-            'id' => 2,
-            'name' => 'DCC',
-            'url' => 'http://www.dcc.ac.uk',
-            'logo' => 'logo-dcc.png',
-            'is_external' => 1,
-            'is_active' => 1
-        ]);
-
-        Institution::create([
-            'id' => 3,
-            'name' => 'DFG',
-            'url' => 'http://www.dfg.de',
-            'logo' => 'logo-dfg.png',
-            'is_external' => 1,
-            'is_active' => 0
-        ]);
-
-        Institution::create([
-            'id' => 4,
-            'name' => 'BMBF',
-            'url' => 'http://www.bmbf.de',
-            'logo' => 'logo-bmbf.png',
-            'is_external' => 1,
-            'is_active' => 0
-        ]);
     }
 }
