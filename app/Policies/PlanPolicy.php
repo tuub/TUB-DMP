6d018cb26d8e9904545729462f644ac7ca6f9249
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2017-04-08 00:59:51 +0200

    [TUBDMP-3] First Gate Policy for plan controller - not working yet.

---
+++ app/Policies/PlanPolicy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Plan;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PlanPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    /**
+     * Determine if the given plan can be displayed by the user.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Plan  $plan
+     * @return bool
+     */
+    public function show(User $user, Plan $plan)
+    {
+        return $user->id === $plan->project->user_id;
+    }
+
+
+    /**
+     * Determine if the given plan can be created by the user.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Plan  $plan
+     * @return bool
+     */
+    public function create(User $user, Plan $plan)
+    {
+        return $user->id === $plan->project->user_id;
+    }
+
+
+    /**
+     * Determine if the given plan can be updated by the user.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Plan  $plan
+     * @return bool
+     */
+    public function update(User $user, Plan $plan)
+    {
+        return $user->id === $plan->project->user_id;
+    }
+}
