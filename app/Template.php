abb5f740d42bb649c7b18ad70b0c86593ce6944a
Fab Fuerste <fab.fuerste@gmail.com>
2018-01-25 23:57:51 +0100

    [TUBDMP-50] Recursion for copy function for templates (fab,mri)
    * kudos to mri for help!

--- app/Template.php
+++ app/Template.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Library\Traits\Uuids;
+use Webpatser\Uuid\Uuid;
 
 class Template extends Model
 {
@@ -71,25 +72,69 @@ class Template extends Model
     |--------------------------------------------------------------------------
     */
 
+    /**
+     *
+     * @author fab
+     * @guru mri
+     *
+     * @param $new_section
+     * @param $original
+     * @param $copy
+     */
+    public static function copyKids($new_section, $original, $copy) {
+        foreach($original->children as $original_child) {
+            $node = $new_section->questions()->create($original_child->toArray());
+            $node->template_id = $copy->template_id;
+            $node->section_id = $copy->section_id;
+            $node->parent_id = $copy->id;
+            $node->save();
+            self::copyKids($new_section, $original_child, $node);
+        }
+    }
+
+
+    /**
+     *
+     * @author fab
+     * @guru mri
+     *
+     * @return bool
+     */
     public function copy()
     {
         $current_template = $this;
         $new_template = $current_template->replicate()->setRelations([]);
 
         if ($new_template) {
-            $new_template->name .= ' - COPY (' . date('Ymdhis') . ')';
+            $new_template->name .= ' - COPY (' . date('YmdHis') . ')';
             $new_template->is_active = 0;
             $new_template->push();
 
             foreach($current_template->sections as $current_section) {
                 $new_section = $new_template->sections()->create($current_section->toArray());
-                foreach($current_section->questions as $current_question) {
-                    $new_question = $new_section->questions()->create($current_question->toArray());
-                    $new_question->template()->associate($new_template->id);
-                    $new_question->save();
+                $next_parent_id = null;
+
+                foreach ($current_section->questions()->orderBy('depth')->get() as $current_question) {
+                    /* We start with the root */
+                    if($current_question->isRoot()) {
+                        /* We create a root question */
+                        $root = $new_section->questions()->create($current_question->toArray());
+
+                        /* We modify the root's attributes */
+                        $root->template_id = $new_template->id;
+                        $root->section_id = $new_section->id;
+                        $root->parent_id = $next_parent_id;
+                        // FIX ME:
+                        // $root->template()->associate($new_template->id);
+
+                        /* We save the root */
+                        $root->save();
+
+                        /* We query the children */
+                        self::copyKids($new_template, $current_question, $root);
+                    }
                 }
             }
-
             return true;
         }
 
