63f74f2781370dafce34bb4e2b7a022c58cdf9a2
Fab Fuerste <fab.fuerste@gmail.com>
2018-02-07 21:35:35 +0100

    [TUBDMP-54] Bugfix: Escape extra break in project request modal

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -30,7 +30,7 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">{{ trans('project.request.title') }}</h4>
-                {{ trans('project.request.subtitle') }}
+                {!! trans('project.request.subtitle') !!}
             </div>
             <div class="modal-body">
                 <div class="errors"></div>
